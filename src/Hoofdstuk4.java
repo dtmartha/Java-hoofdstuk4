3f2f0ac5bb4bbeb1389ad9ef5e4843dae9f26a1c
dilion <dmartha@roc-dev.com>
2016-09-09 22:45:35 +0200

    Hoofdstuk4

--- src/Hoofdstuk4.java
+++ src/Hoofdstuk4.java
@@ -13,7 +13,7 @@ public class Hoofdstuk4 extends Applet {
 
     public void init() {
     }
-/*tabellen*/
+/*Tabellen*/
     public void paint(Graphics g) {
         setBackground(Color.white);
         g.setColor(Color.black);
@@ -24,7 +24,7 @@ public class Hoofdstuk4 extends Applet {
         g.drawString("J", 110,240 );
         g.drawString("H", 170,240 );
 
-        g.drawString("100", 15, 35 );
+        g.drawString("100", 15, 35 ); /*Tabellen*/
         g.drawString("80", 25, 75 );
         g.drawString("60", 25, 115 );
         g.drawString("40", 25, 155 );
@@ -36,21 +36,21 @@ public class Hoofdstuk4 extends Applet {
         g.fillRect(100,35,50,220 - 35);
 
         g.setColor(Color.orange);
-        g.fillRect(150,50,50,170); /*tabellen*/
+        g.fillRect(150,50,50,170); /*Tabellen*/
 
         /*praktijkopdracht*/
         g.setColor(Color.black);
         g.drawLine(400, 60, 270, 60);
-        g.drawString("Lijn",330, 70 ); /*lijn*/
+        g.drawString("Lijn",330, 70 ); /*Lijn*/
 
         g.drawRect(500, 20, 100, 50);
-        g.drawString("Rechthoek",520, 80 ); /*rechthoek*/
+        g.drawString("Rechthoek",520, 80 ); /*Rechthoek*/
 
         g.setColor(Color.magenta);
-        g.fillRect(650, 20, 100, 50); /*rechthoek*/
+        g.fillRect(650, 20, 100, 50); /*Rechthoek*/
         g.setColor(Color.black);
         g.drawOval(650,20, 100, 50); /*ellips*/
-        g.drawString("Gevulde rechthoek met ovaal",630, 80 ); /*rechthoek*/
+        g.drawString("Gevulde rechthoek met ovaal",630, 80 ); /*Rechthoek*/
 
         g.setColor(Color.black);
         g.drawRoundRect(850, 20, 100, 50, 10, 10);
@@ -59,16 +59,16 @@ public class Hoofdstuk4 extends Applet {
         g.setColor(Color.magenta);
         g.fillOval(975,20, 100, 50);
         g.setColor(Color.black);
-        g.drawString("Gevulde Ovaal",985, 80 );
+        g.drawString("Gevulde Ovaal",985, 80 ); /*Gevulde ovaal*/
 
         g.drawOval(1100, 20, 100, 100);
-        g.drawString("Cirkel ",1135, 140 );
+        g.drawString("Cirkel ",1135, 140 ); /*Cirkel*/
 
         g.drawOval(250,175,120,80);
         g.setColor(Color.magenta);
         g.fillArc(250, 175, 120, 80, 90, 45);
         g.setColor(Color.black);
-        g.drawString("Taartpunt met ovaal eromheen ",235, 275 );
+        g.drawString("Taartpunt met ovaal eromheen ",235, 275 ); /*Taartpunt met ovaal eromheen*/
 
 
 
